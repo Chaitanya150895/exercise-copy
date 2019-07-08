d50645ce3141d6f03c43f33d5ba109bdfd3650d9
Chaitanya Sharma <chaitanya150895@gmail.com>
2019-07-08 23:04:51 +0530

    content added and css applied to total.component.html

--- src/app/products.ts
+++ src/app/products.ts
@@ -1,39 +1,42 @@
 export const products = [
     {
-      name: 'PHONE - XL',
-      price: 799,
-      description: 'Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit...',
+      name: 'iPhone XR',
+      price: 1269,
+      description: 'Aerospace-grade aluminum bands. A special Apple-designed alloy is precision-machined to create structural bands, and anodized to complement the colour of the back glass.....',
+      detail:'All-new Liquid Retina display — the most advanced LCD in the industry. Even faster Face ID. The smartest, most powerful chip in a smartphone. And a breakthrough camera system. iPhone XR. It’s beautiful any way you look at it. Introducing Liquid Retina. The new display on iPhone XR is the most advanced LCD in the industry. An innovative backlight design allows the screen to stretch into the corners. So you see true-to-life colour from one beautiful edge to the other. Exceptional materials. The most durable front glass ever in a smartphone. Precision-machined, aerospace-grade aluminum bands. Water and dust resistance.2 And six stunning new finishes.',
       performance: 'Hexa Core',
       display: '5.8 inch (14.73 cm)',
       storage: '64 GB',
       camera: '12 MP + 12 MP',
       battery: '2658 mAh',
-      image: 'http://pluspng.com/img-png/iphone-hd-png-apple-iphone-6-550.png'
+      image: 'https://cdn.shoplightspeed.com/shops/606657/files/12973892/apple-iphone-xr-256gb-white.jpg'
     },
 
 
     {
-      name: 'PHONE - GIANTE',
-      price: 699,
-      description: 'Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit...',
+      name: 'iPhone XS Max',
+      price: 1539,
+      description: 'ï12MP dual cameras with dual OIS and 7MP TrueDepth front camera—Portrait mode, Portrait Lighting, Depth Control and Smart HDR...',
+      detail: 'iPhone Xs Max features a 6.5-inch Super Retina display with custom-built OLED panels for an HDR display that provides the industry’s best colour accuracy, true blacks and remarkable brightness.1 Advanced Face ID lets you securely unlock your iPhone, log in to apps and pay with just a glance. The A12 Bionic chip with next-generation Neural Engine uses real-time machine learning to transform the way you experience photos, gaming, augmented reality and more. A breakthrough 12MP dual-camera system takes your portraits to the next level with Portrait mode, Portrait Lighting, enhanced bokeh and all-new Depth Control. Water resistance.2 And iOS 12—the most advanced mobile operating system—with powerful new tools that make iPhone more personal than ever.',
       performance: 'Octa Core',
       display: '6.2 inch (14.73 cm)',
       storage: '128 GB',
       camera: '18 MP + 12 MP',
       battery: '5435 mAh',
-      image: 'http://pluspng.com/img-png/iphone-hd-png-apple-iphone-png-image-iphone-png-1280.png'
+      image: 'https://cdn.shoplightspeed.com/shops/606657/files/12972783/apple-iphone-xs-max-64gb-space-grey.jpg'
     },
 
 
     {
-      name: 'PHONE STANDARD',
-      price: 299,
-      description: 'Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit...',
+      name: 'iPhone XS',
+      price: 1619,
+      description: 'The display has rounded corners. When measured as a rectangle, the iPhone Xs screen is 5.85 inches diagonally. Actual viewable area is less....',
+      detail:'iPhone Xs is splash-, water- and dust-resistant, and was tested under controlled laboratory conditions with a rating of IP68 under IEC standard 60529 (maximum depth of 2 metres up to 30 minutes). Splash, water, and dust resistance are not permanent conditions, and resistance might decrease as a result of normal wear. Do not attempt to charge a wet iPhone; refer to the user guide for cleaning and drying instructions. Liquid damage not covered under warranty.Apple Pay is not available in all markets. For a list of Apple Pay countries and regions, Qi wireless chargers sold separately.',
       performance: 'Hexa Core',
       display: '5.2 inch',
       storage: '32 GB',
       camera: '12 MP + 8 MP',
       battery: '2658 mAh',
-      image: 'http://pluspng.com/img-png/iphone-hd-png-iphone-images-5684poster-png-804.png'
+      image: 'https://cdn.shoplightspeed.com/shops/606657/files/12972782/apple-iphone-xs-256gb-silver.jpg'
     }
   ];
