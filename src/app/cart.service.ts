520838c12e09571735ef7691d4ae300aa22892df
Chaitanya Sharma <chaitanya150895@gmail.com>
2019-07-22 15:45:23 +0530

    used style="justify-content: center;" to align avatar in center

--- src/app/cart.service.ts
+++ src/app/cart.service.ts
@@ -8,6 +8,15 @@ import { HttpClient } from '@angular/common/http';
 export class CartService {
   items = [];
 
+  customer = {
+    name: '',
+    email: '',
+    password: '',
+    address: '',
+    checkbox: '',
+    shipping_value: ''
+  }
+
   constructor(
     private http: HttpClient
   ) {}
@@ -19,6 +28,25 @@ export class CartService {
   getItems() {
     return this.items;
   }
+
+
+
+  getTotalPrice() {
+    var totalPrice = 0;
+    for (const item of this.items) {
+      totalPrice += item.price;
+    }
+    return totalPrice;
+  }
+
+
+  // getFinalPrice() {
+  //   var tax = 12.5;
+  //   var finalPrice = 0;
+  //   finalPrice = this.getTotalPrice + this.getTax;
+  //   return finalPrice;
+    
+  // }
 
   clearCart() {
     this.items = [];
